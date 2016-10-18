320cdc7484aea2b2d0a68fe2582b3a93162ba42b
kevm66 <kevm66@gmail.com>
2016-10-18 09:22:47 +0000

    initial commit

--- login.php
+++ login.php
@@ -1,64 +1,80 @@
-<?php 
-  include_once 'dbconnect.php';
-session_start();
-   
-   
-   if(isset($_SESSION['user'])!="")
-{
- echo "<script>alert('You are already logged in');</script>";
- header( 'Location: index.php');
-}
-
-if(isset($_POST['btn-login']))
-{
- $username = mysql_real_escape_string($_POST['username']);
- $password = mysql_real_escape_string($_POST['password']);
- $res=mysql_query("SELECT * FROM user WHERE username='$username'");
- $row=mysql_fetch_array($res);
- 
- if($row['password']==md5($password))
- {
-  $_SESSION['user'] = $row['username'];
-  
-  header( 'Location: /index.php');
-  echo "<script type='text/javascript'>alert('You have now logged-in.');</script>";
- }
- else
- {
-  ?>
-        <script>alert('Wrong details entered, please try again.');</script>
- echo "<script>window.location = 'login.php';</script>";
-?>
 <?php
- }
-}
-
+   include("dbconnect.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      //$myusername = mysqli_real_escape_string($db,$_POST['username']);
+      //$mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+     // $result = mysqli_query($db,$sql);
+    //  $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
 ?>
-<div class="mdl-grid">
-    <div class="mdl-cell mdl-cell--4-col"></div>
-    <div class="mdl-cell mdl-cell--4-col mdl-shadow--2dp">
-      <section class="login-register">
- <h3>User Login</h3>
-        <hr/>
-        
-        <!-- login form start -->
-        <form id="loginform" action="" method="post">
-          <!-- row 1 -->
-          <label for="username">Username/Email:</label><br/>
-          <input type="text" name="email" class="required" "mdl-textfield__input" placeholder="Email"/></p>
-          
-          <!-- row 2 -->
-          <label for="password">Password:</label><br/>
-          <input type="password" name="password" class="required" "mdl-textfield__input" placeholder="password" /></br></br>
-          
-          <!-- row 3 -->
-          <button type="submit" name="btn-login" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">Login</button></br></br>
-          
-          <label for="remember" class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
-              <input type="checkbox" value="remember" id="remember" class="mdl-checkbox__input">
-              <span class="mdl-checkbox__label">Stay logged-in</span>
-          </label></p>
-          
-          <p><a href="forget.php" onclick="forget.php" id="forget">Forgot Password?</a></p>
-          
-        </form>
+<html>
+   
+   <head>
+      <title>Login Page</title>
+      
+      <style type = "text/css">
+         body {
+            font-family:Arial, Helvetica, sans-serif;
+            font-size:24px;
+            background-color:#0000;
+         }
+         
+         label {
+            font-weight:bold;
+            width:100px;
+            font-size:14px;
+            background-color: #D1C6C4;
+         }
+         
+         .box {
+            border:#666666 solid 1px;
+         }
+      </style>
+      
+   </head>
+   
+   <body bgcolor = "#FFFFFF">
+	
+      <div align = "center" margin-top:"30px;">
+         <div style = "width:300px; border: solid 1px #333333; " align = "left;" margin-top:"30px;">
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+				
+            <div style = "margin:30px">
+               
+               <form action = "" method = "post">
+                  <label>User Name  :</label><input type = "text" name = "Username" class = "box"/><br /><br />
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <input type = "submit" value = " Login"/><br />
+                  <a href="#" class="home_btn" onclick="location.reload();location.href='index.html'">go back</a>
+               </form>
+               
+               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+					
+            </div>
+				
+         </div>
+			
+      </div>
+
+   </body>
+</html>
