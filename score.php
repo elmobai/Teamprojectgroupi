963879e3143177d6dc543000ca089d26d8a731b7
kevm66 <kevm66@gmail.com>
2016-12-15 15:41:59 +0000

    fixed overview bugs, modified welcome message

--- score.php
+++ score.php
@@ -4,6 +4,7 @@
 <?php include_once 'api/DbBookings.php';?>
 
 
+
 <?php 
 if(isset($_SESSION['user'])=="")
 {
@@ -14,23 +15,44 @@ if(isset($_SESSION['user'])=="")
 </br>
  
 <div class="container">
-   <h1><?php echo $_SESSION['user']; ?>'s Profile</h1>
+    
+    <!--welcome-->
+    <h1><?php 
+              if(isset($_SESSION['user'])!=""){ 
+                ?>
+              
+                  Welcome <?php if(isset($_SESSION['user'])=="")
+                    	{
+                    	 echo "<script>alert('You must log in first to continue!');</script>";
+                    	 echo "<script>window.location = 'https://teamprojectgroupi-elmobai.c9users.io/login.php';</script>";
+                    	}
+                    	$db2 = Connect();
+                    	$user = $_SESSION['user'];
+                    	$sql44 = "SELECT firstname FROM registered WHERE email = '".$user."' ";
+                         if ($result23 = $db2->query($sql44)){
+                            if($row3 = $result23->fetch_assoc()){
+                                $firstname2 = $row3['firstname']; //fetch ("$row ")
+                            	echo $firstname2;
+                            }?>!</font></strong>
+                        <?php }
+              } ?></h1>
    
-   <div id="my Stats" class="row">
+   <!-- row of 3 columns -->
+   <div class="row">
    
     <!-- separate divs -->
     </p>
    
-    <!-- statistics overview --> 
-    <div id="graph" class="col-sm-4">
+    <!-- statistics overview col --> 
+    <div class="col-sm-4">
 
          <section class="login-register">
-          <!-- <h1>Graph</h1></br> -->
           <ul class="list-group">
-             <h1>Statistics Overview</h1></br>
-             <p>Your results are listed below</p></br></br>
+            <h1>Statistics Overview</h1></br>
+            <p>Your results are listed below</p></br></br>
              
-             <h4><li class="list-group-item">Games I've Played: 
+            <!--games i've played-->
+            <h4><li class="list-group-item">Games I've Played: 
                  
                  <?php if(isset($_SESSION['user'])=="")
                 	{
@@ -44,50 +66,59 @@ if(isset($_SESSION['user'])=="")
                         $count = $result2->num_rows; //fetch ("$row ")
                     	echo $count;
                     }
-                ?></li></h4>
-              <!-- <?php if($row = mysql_fetch_array("SELECT AVERAGE FROM scores WHERE user = '".$user."'")){?>-->
+                ?>
+            </li></h4>
+                
+                
+             <!-- <?php if($row = mysql_fetch_array("SELECT AVERAGE FROM scores WHERE user = '".$user."'")){?>-->
+              
+             <!--average score--> 
              <h4><li class="list-group-item">Average score: <?php echo $row['AVERAGE'];?> points</li></h4>
              
-             <!--
-             SELECT    score1,
-                       score2,
-                       score3,
-                       score4,
-                       score5,
-                      (score1 + score2 +  score3 + score4 + score5 )   as ‘AVERAGE’
-                      FROM scores
-            -->
-             <?php }?>
-             <?php if($row = mysql_fetch_array("")){?>
-             <h4><li class="list-group-item">Best game: 50 points</li></h4>
-             <!--SELECT MAX( AVERAGE ) 
-             FROM scores
-                       
-             -->
-              <?php }?>
-              <?php if($row = mysql_fetch_array("")){?>
-             <h4><li class="list-group-item">Worst game: 5 point </li></h4>
-             <!--
-             SELECT MIN( AVERAGE ) 
-             FROM scores
+                 <!--
+                 SELECT    score1,
+                           score2,
+                           score3,
+                           score4,
+                           score5,
+                          (score1 + score2 +  score3 + score4 + score5 )   as ‘AVERAGE’
+                          FROM scores
                 -->
-                    <?php }?>    
-                    <?php if($row = mysql_fetch_array("")){?>
-                    
+                 <?php }?>
+                 <?php if($row = mysql_fetch_array("")){?>
+             
+             <!--best game-->
+             <h4><li class="list-group-item">Best game: 50 points</li></h4>
+                 <!--SELECT MAX( AVERAGE ) 
+                 FROM scores
+                           -->
+                  <?php }?>
+                  <?php if($row = mysql_fetch_array("")){?>
+              
+             <!--worst game--> 
+             <h4><li class="list-group-item">Worst game: 5 point </li></h4>
+                 <!--
+                 SELECT MIN( AVERAGE ) 
+                 FROM scores
+                    -->
+                <?php }?>    
+                <?php if($row = mysql_fetch_array("")){?>
+              
+             <!--most recent game-->       
              <h4><li class="list-group-item">Most recent game: 13/12/2016</li></h4>
-              <?php }?><!-- TIMESTAMP WITH ADD DATETIME ECHO DATE TIME HERE (DATE_SUB)
-              <?php if($row = mysql_fetch_array("")){?>
-             <h4><li class="list-group-item">Most recent score: 13/12/2-16</li></h4>
-                <?php }?>
+                 <?php }?><!-- TIMESTAMP WITH ADD DATETIME ECHO DATE TIME HERE (DATE_SUB)-->
+                 <?php if($row = mysql_fetch_array("")){?>
+              
+             <?php }?>
+             
            </ul>  
         </section>
-      </diiv>
+        
     </div>
       
     <!-- separate divs -->
     </p>
     
-   
     <!-- add score -->  
     <div id="target" class="col-sm-4"> 
     <section class="login-register">
@@ -123,21 +154,21 @@ if(isset($_SESSION['user'])=="")
               $sql = "SELECT * from scores WHERE user = '$user_id'";
               $queryAdd = mysql_query($sql) or die(mysql_error());
          
-          $res = json_decode($result, true);
-          //foreach($res as $item); //foreach element in $arr
-          while ($row = mysql_fetch_array($queryAdd)) {?>
+              $res = json_decode($result, true);
+              //foreach($res as $item); //foreach element in $arr
+              while ($row = mysql_fetch_array($queryAdd)) {?>
           
                <ul class='list-group'>
                 <li class='list-group-item'>
                         
-                  <p name="id">id: <?php echo $row['id'];?> </p></br>
+                  <!--<p name="id">id: <?php echo $row['id'];?> </p></br>-->
                   <h3>Round: <?php echo $row['round'];?> </h3></br>
                   <h4>Shot 1: <?php echo $row['score1'];?> </h4>
                   <h4>Shot 2: <?php echo $row['score2'];?>  </h4>
                   <h4>Shot 3: <?php echo $row['score3'];?>  </h4>
                   <h4>Shot 4: <?php echo $row['score4'];?>  </h4>
                   <h4>Shot 5: <?php echo $row['score5'];?>  </h4>
-                  <h4>Total: <?php echo  ($row['score3']) + ($row['score2']) + ($row['score1']) + ($row['score4']) + ($row['score5']);?> </h4></br>
+                  <span><h4>Total: <?php echo  ($row['score3']) + ($row['score2']) + ($row['score1']) + ($row['score4']) + ($row['score5']);?> </h4></span></br>
                   
                   <!--
                      add button to here
